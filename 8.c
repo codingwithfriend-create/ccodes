27a0f7791a3306fdd90b1376b8699ee079027524
rachit <codingwithfriend@gmail.com>
2026-01-30 20:32:48 +0530

    intialize push

--- 8.c
+++ 8.c
@@ -15,30 +15,56 @@
 //     return 0;
 // }
 
+// #include <stdio.h>
+
+// int main() {
+//     int marks;
+
+//     printf("Enter your marks: ");
+//     scanf("%d", &marks);
+
+
+//     if (marks > 100 || marks < 0) {
+//         printf("Invalid input! Please enter marks between 0 and 100.\n");
+//     } 
+//     else if (marks >= 75) {
+//         printf("Grade: A\n");
+//     } 
+//     else if (marks >= 50) {
+//         printf("Grade: B\n");
+//     } 
+//     else if (marks >= 35) {
+//         printf("Grade: C\n");
+//     } 
+//     else {
+        
+//         printf("Grade: D\n");
+//     }
+
+//     return 0;
+// }
 #include <stdio.h>
 
-int main() {
-    int marks;
+int main() 
+{
+    char c;
 
-    printf("Enter your marks: ");
-    scanf("%d", &marks);
+    printf("Enter your character: ");
+    scanf("%c", &c);
 
-    // Starting the if-else ladder
-    if (marks > 100 || marks < 0) {
-        printf("Invalid input! Please enter marks between 0 and 100.\n");
+
+    if (c>='A' && c<='Z') {
+        printf("UPPER CHAR\n");
     } 
-    else if (marks >= 75) {
-        printf("Grade: A\n");
+    else if (c>='a' && c<='z') {
+        printf("LOWER CHAR\n");
     } 
-    else if (marks >= 50) {
-        printf("Grade: B\n");
-    } 
-    else if (marks >= 35) {
-        printf("Grade: C\n");
+    else if (c>='0' && c<='9') {
+        printf("NUM\n");
     } 
     else {
         
-        printf("Grade: D\n");
+        printf("SPECIAL CHAR\n");
     }
 
     return 0;
