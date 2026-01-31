000cadea6fc0405b4859835e04eaf3071ef4ba07
rachit <codingwithfriend@gmail.com>
2026-01-31 09:16:55 +0530

    commit

---
+++ 2510990849_if assignment.c
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+int main()
+{
+int s ,s2;
+printf("enter base and height of the triangle");
+scanf("%d %d",&s,&s2);
+float a=.5*(s*s2);
+printf("Area of the triangle is :%.2f",a );
+return 0;
+}
