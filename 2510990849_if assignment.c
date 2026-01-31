18b323b613598a5f1aac48d51759dad71532670d
rachit <codingwithfriend@gmail.com>
2026-01-31 09:22:24 +0530

    commit

--- 2510990849_if assignment.c
+++ 2510990849_if assignment.c
@@ -1,11 +1,26 @@
-
+// // Q1
 #include<stdio.h>
 int main()
+// {
+// int s ,s2;
+// printf("enter base and height of the triangle");
+// scanf("%d %d",&s,&s2);
+// float a=.5*(s*s2);
+// printf("Area of the triangle is :%.2f",a );
+// return 0;
+// }
+
+//Q2
 {
-int s ,s2;
-printf("enter base and height of the triangle");
-scanf("%d %d",&s,&s2);
-float a=.5*(s*s2);
-printf("Area of the triangle is :%.2f",a );
-return 0;
+    int a;
+    int b;
+    printf("enter a 5 digit no.");
+    scanf("%d",&a);
+    for(int i=0;i<5;i++)
+    {
+        b=(int)b*10+a%10;
+        a=(int)a/10;
+    }
+    printf("the reversed no is: %d",b);
+    return 0;
 }
