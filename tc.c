c7d1ee8fbba6f07d24eaefe9eb89f37eff77718b
rachit <codingwithfriend@gmail.com>
2026-02-06 11:55:47 +0530

    commit

--- tc.c
+++ tc.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<ctype.h>
 int main()
 // {
 //     double x=1.2;
@@ -6,19 +7,35 @@ int main()
 //     printf("sum=%d",sum);
 //     return 0;
 // }
+// {
+//     int a=0;
+//     scanf("%d",&a);
+//     switch(a)
+//     {
+//         case 1:printf("monday\n");break;
+//         case 2:printf("tuesday\n");break;
+//         case 3:printf("wednesday\n");break;
+//         case 4:printf("thursday\n");break;
+//         case 5:printf("friday\n");break;
+//         case 6:printf("saturdau\n");break;
+//         case 7:printf("sunday\n");break;
+//         default:printf("invalid");
+//     }
+//     return 0;
+// }
+// {
+
+//     int a=0;
+//     scanf("%d",&a);
+//     switch(a)
+//     {
+//         case 1:printf("monday\n");
+//         case 2:printf("tuesday\n");break;
+//     return 0;
+
+// }
+// }
 {
-    int a=0;
-    scanf("%d",&a);
-    switch(a)
-    {
-        case 1:printf("monday\n");break;
-        case 2:printf("tuesday\n");break;
-        case 3:printf("wednesday\n");break;
-        case 4:printf("thursday\n");break;
-        case 5:printf("friday\n");break;
-        case 6:printf("saturdau\n");break;
-        case 7:printf("sunday\n");break;
-        default:printf("invalid");
-    }
+
     return 0;
 }
