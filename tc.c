38d4cd4041c4b680d19d79e7f6518dde8230b037
rachit <codingwithfriend@gmail.com>
2026-02-06 11:34:29 +0530

    commit

--- tc.c
+++ tc.c
@@ -11,9 +11,14 @@ int main()
     scanf("%d",&a);
     switch(a)
     {
-        case 1:printf("1");break;
-        case 2:printf("2");break;
-        default:printf("ppp");
+        case 1:printf("monday\n");break;
+        case 2:printf("tuesday\n");break;
+        case 3:printf("wednesday\n");break;
+        case 4:printf("thursday\n");break;
+        case 5:printf("friday\n");break;
+        case 6:printf("saturdau\n");break;
+        case 7:printf("sunday\n");break;
+        default:printf("invalid");
     }
     return 0;
 }
