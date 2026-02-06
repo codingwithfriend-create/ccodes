290def640e9931a545ca3b9a4ab252d852c9b54b
rachit <codingwithfriend@gmail.com>
2026-02-06 11:32:00 +0530

    commit

--- tc.c
+++ tc.c
@@ -1,8 +1,19 @@
 #include<stdio.h>
 int main()
+// {
+//     double x=1.2;
+//     int sum=(int)x+1;
+//     printf("sum=%d",sum);
+//     return 0;
+// }
 {
-    double x=1.2;
-    int sum=(int)x+1;
-    printf("sum=%d",sum);
+    int a=0;
+    scanf("%d",&a);
+    switch(a)
+    {
+        case 1:printf("1");break;
+        case 2:printf("2");break;
+        default:printf("ppp");
+    }
     return 0;
 }
