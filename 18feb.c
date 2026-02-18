1d2bef1f4e718bb44b8d65e00f6035b9d9eb01cc
rachit <codingwithfriend@gmail.com>
2026-02-18 12:21:51 +0530

    commit

--- 18feb.c
+++ 18feb.c
@@ -1,87 +1,87 @@
-// // table
-// #include<stdio.h>
-// int main()
-// {
-//     int a[5][5];
-//     for(int i=0;i<5;i++)
-//     {
-//         for(int j=0;j<5;j++)
-//         {
-//             a[i][j]=(i+1)*(j+1);
-//         }
-//     }
-//     printf(". |  1. 2. 3. 4. 5.\n ");
-//     printf("-------------------\n");
-//     for (int i = 0; i < 5; i++)
-//      { 
-//         printf("%d |", (i + 1));
-//         for (int j = 0; j < 5; j++) 
-//         { 
+// table
+#include<stdio.h>
+int main()
+{
+    int a[5][5];
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            a[i][j]=(i+1)*(j+1);
+        }
+    }
+    printf(". |  1. 2. 3. 4. 5.\n ");
+    printf("-------------------\n");
+    for (int i = 0; i < 5; i++)
+     { 
+        printf("%d |", (i + 1));
+        for (int j = 0; j < 5; j++) 
+        { 
    
-//             printf(" %2d", a[i][j]); 
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
+            printf(" %2d", a[i][j]); 
+        }
+        printf("\n");
+    }
+    return 0;
+}
 // add
-// #include<stdio.h>
-// int main()
-// {
-//     int a[5][5];
-//     int b[5][5];
-//     int c[5][5];
-//     for(int i=0;i<5;i++)
-//     {
-//         for(int j=0;j<5;j++)
-//         {
-//             scanf("%d",&a[i][j]);
-//             scanf("%d",&b[i][j]);
-//             c[i][j]=a[i][j]+b[i][j];
-//         }
-//     }
-//     printf("c matrix after addition of a and b is\n ");
-//     printf("-------------------\n");
-//     for (int i = 0; i < 5; i++)
-//      { 
-//         for (int j = 0; j < 5; j++) 
-//         { 
+#include<stdio.h>
+int main()
+{
+    int a[5][5];
+    int b[5][5];
+    int c[5][5];
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            scanf("%d",&a[i][j]);
+            scanf("%d",&b[i][j]);
+            c[i][j]=a[i][j]+b[i][j];
+        }
+    }
+    printf("c matrix after addition of a and b is\n ");
+    printf("-------------------\n");
+    for (int i = 0; i < 5; i++)
+     { 
+        for (int j = 0; j < 5; j++) 
+        { 
    
-//             printf(" %5d", c[i][j]); 
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
-// // substract
-// #include<stdio.h>
-// int main()
-// {
-//     int a[5][5];
-//     int b[5][5];
-//     int c[5][5];
-//     for(int i=0;i<5;i++)
-//     {
-//         for(int j=0;j<5;j++)
-//         {
-//             scanf("%d",&a[i][j]);
-//             scanf("%d",&b[i][j]);
-//             c[i][j]=a[i][j]-b[i][j];
-//         }
-//     }
-//     printf("c matrix after substraction of a and b is\n ");
-//     printf("-------------------\n");
-//     for (int i = 0; i < 5; i++)
-//      { 
-//         for (int j = 0; j < 5; j++) 
-//         { 
+            printf(" %5d", c[i][j]); 
+        }
+        printf("\n");
+    }
+    return 0;
+}
+// substract
+#include<stdio.h>
+int main()
+{
+    int a[5][5];
+    int b[5][5];
+    int c[5][5];
+    for(int i=0;i<5;i++)
+    {
+        for(int j=0;j<5;j++)
+        {
+            scanf("%d",&a[i][j]);
+            scanf("%d",&b[i][j]);
+            c[i][j]=a[i][j]-b[i][j];
+        }
+    }
+    printf("c matrix after substraction of a and b is\n ");
+    printf("-------------------\n");
+    for (int i = 0; i < 5; i++)
+     { 
+        for (int j = 0; j < 5; j++) 
+        { 
    
-//             printf(" %5d", c[i][j]); 
-//         }
-//         printf("\n");
-//     }
-//     return 0;
-// }
+            printf(" %5d", c[i][j]); 
+        }
+        printf("\n");
+    }
+    return 0;
+}
 // scaler multiplication
 #include<stdio.h>
 int main()
