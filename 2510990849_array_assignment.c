c6090b816f98ed5632d0099c3e3079466645e130
rachit <codingwithfriend@gmail.com>
2026-02-26 12:21:35 +0530

    commit

--- 2510990849_array_assignment.c
+++ 2510990849_array_assignment.c
@@ -1,179 +1,179 @@
-1.
+// 1.
 
-#include <stdio.h>
-
- 
-
-int main() {
-
-   int m, n, i, j, found = 0;
-
-   scanf("%d", &m);
-
-   int arr[m];
-
-   for(i = 0; i < m; i++) scanf("%d", &arr[i]);
-
-   scanf("%d", &n);
-
-   
-
-   for(i = 0; i < m; i++) {
-
-       for(j = i + 1; j < m; j++) {
-
-           if(arr[i] + arr[j] == n) {
-
-               printf("Lucky Pair: (%d, %d)\n", arr[i], arr[j]);
-
-               found = 1;
-
-           }
-
-       }
-
-   }
-
-   if(!found) printf("No Lucky Pair found\n");
-
-   return 0;
-
-}
-
-2.
-
-#include <stdio.h>
+// #include <stdio.h>
 
  
 
-int main() {
+// int main() {
 
-   int n, i;
+//    int m, n, i, j, found = 0;
 
-   scanf("%d", &n);
+//    scanf("%d", &m);
 
-   int freq[7] = {0};
+//    int arr[m];
 
-   for(i = 0; i < n; i++) {
+//    for(i = 0; i < m; i++) scanf("%d", &arr[i]);
 
-       int x; scanf("%d", &x);
+//    scanf("%d", &n);
 
-       freq[x]++;
+   
 
-   }
+//    for(i = 0; i < m; i++) {
 
-   for(i = 1; i <= 6; i++) {
+//        for(j = i + 1; j < m; j++) {
 
-       if(freq[i]) printf("%d:%d\n", i, freq[i]);
+//            if(arr[i] + arr[j] == n) {
 
-   }
+//                printf("Lucky Pair: (%d, %d)\n", arr[i], arr[j]);
 
-   return 0;
+//                found = 1;
 
-}
+//            }
 
-3.
+//        }
 
-#include <stdio.h>
+//    }
+
+//    if(!found) printf("No Lucky Pair found\n");
+
+//    return 0;
+
+// }
+
+// 2.
+
+// #include <stdio.h>
 
  
 
-int main() {
+// int main() {
 
-   int n, k, i;
+//    int n, i;
 
-   scanf("%d %d", &n, &k);
+//    scanf("%d", &n);
 
-   k %= n;
+//    int freq[7] = {0};
 
-   int arr[n];
+//    for(i = 0; i < n; i++) {
 
-   for(i = 0; i < n; i++) scanf("%d", &arr[i]);
+//        int x; scanf("%d", &x);
 
-   
+//        freq[x]++;
 
-   for(i = n - k; i < n; i++) printf("%d ", arr[i]);
+//    }
 
-   for(i = 0; i < n - k; i++) printf("%d ", arr[i]);
+//    for(i = 1; i <= 6; i++) {
 
-   printf("\n");
+//        if(freq[i]) printf("%d:%d\n", i, freq[i]);
 
-   return 0;
+//    }
 
-}
+//    return 0;
 
-4.
+// }
 
-#include <stdio.h>
+// 3.
 
- 
-
-int main() {
-
-   int gems[20], target, i, count = 0;
-
-   for(i = 0; i < 20; i++) scanf("%d", &gems[i]);
-
-   scanf("%d", &target);
-
-   
-
-   for(i = 0; i < 20; i++) {
-
-       if(gems[i] == target) count++;
-
-   }
-
-   
-
-   if(count) printf("The gemstone appears %d times.\n", count);
-
-   else printf("The gemstone is not in the collection.\n");
-
-   return 0;
-
-}
-
-5.
-
-#include <stdio.h>
+// #include <stdio.h>
 
  
 
-int main() {
+// int main() {
 
-   int rain[7], i, total = 0, dry = 0, flash = 0;
+//    int n, k, i;
 
-   for(i = 0; i < 7; i++) {
+//    scanf("%d %d", &n, &k);
 
-       scanf("%d", &rain[i]);
+//    k %= n;
 
-       total += rain[i];
+//    int arr[n];
 
-       if(rain[i] == 0) dry = 1;
-
-       if(rain[i] > 100) flash = 1;
-
-   }
+//    for(i = 0; i < n; i++) scanf("%d", &arr[i]);
 
    
 
-   printf("Total Rainfall: %d mm\n", total);
+//    for(i = n - k; i < n; i++) printf("%d ", arr[i]);
 
-   printf("Average Daily Rainfall: %d mm\n", total / 7);
+//    for(i = 0; i < n - k; i++) printf("%d ", arr[i]);
+
+//    printf("\n");
+
+//    return 0;
+
+// }
+
+// 4.
+
+// #include <stdio.h>
+
+ 
+
+// int main() {
+
+//    int gems[20], target, i, count = 0;
+
+//    for(i = 0; i < 20; i++) scanf("%d", &gems[i]);
+
+//    scanf("%d", &target);
 
    
 
-   if(total > 500) printf("Heavy Rainfall Alert\n");
+//    for(i = 0; i < 20; i++) {
 
-   if(flash) printf("Flash Flood Warning\n");
+//        if(gems[i] == target) count++;
 
-   if(total == 0) printf("Drought Alert\n");
-
-   else if(dry) printf("Dry Spell Detected\n");
+//    }
 
    
 
-   return 0;
+//    if(count) printf("The gemstone appears %d times.\n", count);
 
-}
+//    else printf("The gemstone is not in the collection.\n");
+
+//    return 0;
+
+// }
+
+// 5.
+
+// #include <stdio.h>
+
+ 
+
+// int main() {
+
+//    int rain[7], i, total = 0, dry = 0, flash = 0;
+
+//    for(i = 0; i < 7; i++) {
+
+//        scanf("%d", &rain[i]);
+
+//        total += rain[i];
+
+//        if(rain[i] == 0) dry = 1;
+
+//        if(rain[i] > 100) flash = 1;
+
+//    }
+
+   
+
+//    printf("Total Rainfall: %d mm\n", total);
+
+//    printf("Average Daily Rainfall: %d mm\n", total / 7);
+
+   
+
+//    if(total > 500) printf("Heavy Rainfall Alert\n");
+
+//    if(flash) printf("Flash Flood Warning\n");
+
+//    if(total == 0) printf("Drought Alert\n");
+
+//    else if(dry) printf("Dry Spell Detected\n");
+
+   
+
+//    return 0;
+
+// }
